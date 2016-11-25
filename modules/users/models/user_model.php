99ef02935301470d73470a9f4b2f870f75695f60
hfox <381805686@qq.com>
2016-11-25 10:34:14 +0800

    登录、配置

--- modules/users/models/user_model.php
+++ modules/users/models/user_model.php
@@ -1,6 +1,6 @@
 <?php (defined('BASEPATH')) OR exit('No direct script access allowed');
 
-class user_model extends MY_Model {
+class User_model extends MY_Model {
 
     public function __construct()
     {
