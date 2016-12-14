addcb2866212deae8ee2e901f3b8916fe9f7ecd3
yiluo <1585680807@qq.com>
2016-12-14 10:30:10 +0800

    对比分析-雷达图bug修复

--- modules/museum/controllers/Generality.php
+++ modules/museum/controllers/Generality.php
@@ -75,7 +75,7 @@ class Generality extends REST_Controller{
                     if(array_key_exists("micro",$compliance[$value["mid"]])){
                         $abnormal_micro = array_key_exists("abnormal",$compliance[$value["mid"]]["micro"])?array_sum($compliance[$value["mid"]]["micro"]["abnormal"]):0;
                         $total_micro = array_key_exists("total",$compliance[$value["mid"]]["micro"])?array_sum($compliance[$value["mid"]]["micro"]["total"]):0;
-                        $data["value"][] = $micro_compliance[] = $total_micro ? round(($total_micro - $abnormal_micro) / $total_micro, 2) : 0;
+                        $data["value"][] = $micro_compliance[] = $total_micro ? round(($total_micro - $abnormal_micro) / $total_micro, 4)*100 : 0;
                     }else{
                         $data["value"][] = 0;
                     }
@@ -84,7 +84,7 @@ class Generality extends REST_Controller{
                     if(array_key_exists("small",$compliance[$value["mid"]])) {
                         $abnormal = array_key_exists("abnormal",$compliance[$value["mid"]]["small"])?array_sum($compliance[$value["mid"]]["small"]["abnormal"]):0;
                         $total = array_key_exists("total",$compliance[$value["mid"]]["small"])?array_sum($compliance[$value["mid"]]["small"]["total"]):0;
-                        $data["value"][] = $small_compliance[] = $total ? round(($total - $abnormal) / $total, 2) : 0;
+                        $data["value"][] = $small_compliance[] = $total ? round(($total - $abnormal) / $total, 4)*100 : 0;
                     }else{
                         $data["value"][] = 0;
                     }
