898c8e6bdb8f8cd28de9470fb871bc381791cd84
yangbin <619280492@qq.com>
2016-12-23 11:14:32 +0800

    历史达标情况-折线图数据*100

--- modules/analysis/controllers/History.php
+++ modules/analysis/controllers/History.php
@@ -83,7 +83,7 @@ class History extends MY_Controller{
                     ->get("data_complex")
                     ->result_array();
                 if($dc_datas && $dc_datas[0]['total']) //include sp=0
-                    $datas[$mid][$date] = round($dc_datas[0]['standard_percent'],4);
+                    $datas[$mid][$date] = round($dc_datas[0]['standard_percent'],4)*100;
                 else $datas[$mid][$date] = null;
             }
             $names[] = $this->museum[$mid];
@@ -117,11 +117,11 @@ class History extends MY_Controller{
                     ->get("data_complex")
                     ->result_array();
                 if($dc_datas && $dc_datas[0]['scatter_temperature'] != 0) //排除离散为0的空数据
-                    $tc_datas[$mid][$date] = (float)$dc_datas[0]['scatter_temperature'];
+                    $tc_datas[$mid][$date] = (float)$dc_datas[0]['scatter_temperature']*100;
                 else $tc_datas[$mid][$date] = null;
 
                 if($dc_datas && $dc_datas[0]['scatter_humidity'] != 0)
-                    $hc_datas[$mid][$date] = (float)$dc_datas[0]['scatter_humidity'];
+                    $hc_datas[$mid][$date] = (float)$dc_datas[0]['scatter_humidity']*100;
                 else $hc_datas[$mid][$date] = null;
             }
             $names[] = $this->museum[$mid];
